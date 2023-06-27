9b073f811c2f26d9bf84b5f2d2e9281b88369894
Shariff Faleel <shariff.mfa@outlook.com>
2023-06-27 14:46:22 -0700

    Fix reparent fixed position & allow children also to be matched individually

--- Assets/Scripts/HPUI/Utils/ReparentFixedTransform.cs
+++ Assets/Scripts/HPUI/Utils/ReparentFixedTransform.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ubc.ok.ovilab.HPUI.utils.Extensions;
 
 namespace ubc.ok.ovilab.HPUI.Core
 {
@@ -17,20 +18,53 @@ namespace ubc.ok.ovilab.HPUI.Core
         public Transform source;
         public Transform relativeObject;
         public Transform targetSource;
+        public Transform targetRelativeObject;
 
+        public bool matchChildren = false;
+
+        private Dictionary<Transform, Transform> matchingList;
         // Update is called once per frame
         public static void Apply(Transform source,
                                  Transform relativeObject,
                                  Transform targetSource,
                                  Transform targetRelativeObject)
         {
-            targetRelativeObject.transform.position = targetSource.TransformPoint(source.InverseTransformPoint(relativeObject.position));
-            targetRelativeObject.transform.rotation = relativeObject.rotation * (targetSource.rotation * Quaternion.Inverse(source.rotation));
+            // TODO: Consider using the mulitple functions instead of single operations?
+            targetRelativeObject.position = targetSource.TransformPoint(source.InverseTransformPoint(relativeObject.position));
+            Vector3 up = targetSource.TransformDirection(source.InverseTransformDirection(relativeObject.up));
+            Vector3 forward = targetSource.TransformDirection(source.InverseTransformDirection(relativeObject.forward));
+            targetRelativeObject.transform.rotation = Quaternion.LookRotation(forward, up);
+        }
+
+
+        void Start()
+        {
+            matchingList = new Dictionary<Transform, Transform>();
+            if (matchChildren)
+            {
+                Transform[] targetTransforms = targetRelativeObject.GetComponentsInChildren<Transform>();
+                foreach(Transform t in targetTransforms)
+                {
+                    Transform otherT = relativeObject.FindDescendentTransform(t.name);
+                    if (otherT != null)
+                    {
+                        matchingList.Add(t, otherT);
+                    }
+                }
+            }
         }
 
         void Update()
         {
-            ReparentFixedTransform.Apply(source, relativeObject, targetSource, this.transform);
+            ReparentFixedTransform.Apply(source, relativeObject, targetSource, targetRelativeObject);
+
+            if (matchChildren)
+            {
+                foreach(KeyValuePair<Transform, Transform> kvp in matchingList)
+                {
+                    ReparentFixedTransform.Apply(source, kvp.Value, targetSource, kvp.Key);
+                }
+            }
         }
     }
 }
