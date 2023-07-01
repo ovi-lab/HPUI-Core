13d0dabae640d7915a103649303149d0c49c80ee
Shariff Faleel <shariff.mfa@outlook.com>
2023-06-30 17:25:37 -0700

    Update alglib & accurater rotation when two parents in trasformlinker

--- Assets/Scripts/HPUI/Core/TransformLinker.cs
+++ Assets/Scripts/HPUI/Core/TransformLinker.cs
@@ -46,8 +46,55 @@ namespace ubc.ok.ovilab.HPUI.Core
                     }
                 }
             }
-        
 	}
+
+        private Vector3 GetCombiendDirection(Vector3 p1, Vector3 p2, Vector3 direction1, Vector3 direction2, Vector3 direction3)
+        {
+            // p1 + t1*d1 + t3*d3 = t2*p2 + p2
+            // t1 * d1 - t2 * d2 + t3 * d3 = p2 - p1
+
+            Vector3 b = p2 - p1;
+            double[,] A = new double[3, 3] {
+                { direction1.x, -direction2.x, direction3.x },
+                { direction1.y, -direction2.y, direction3.y },
+                { direction1.z, -direction2.z, direction3.z }
+            };
+            double[] B = new double[3] { b.x, b.y, b.z };
+            double[] result;
+            alglib.densesolverreport report;
+            alglib.rmatrixsolve(A, 3, B, out result, out report);
+
+            if (report.terminationtype <= 0)
+            {
+                return direction1;
+            }
+            float t1 = (float)result[0];
+            float t3 = (float)result[2];
+
+            // Quick tests:
+            // Debug.Log($"{result[0]} {result[1]}  {result[2]}");
+            // GetCombiendDirection(Vector3.zero, new Vector3(6, -4, 27), new Vector3(1, 0, 2), new Vector3(-1, -2, -5), new Vector3(1, 5, -1));
+            // should print 5, 3, -2
+            // GetCombiendDirection(new Vector3(0, 0, 0), new Vector3(-4, 14, -2), new Vector3(2, 3, 5), new Vector3(-3, 2, -1), new Vector3(-4,  5, -3));
+            // should print 1, 2, 3
+
+            Vector3 targetPoint = p1 + t1 * direction1 + 0.5f * t3 * direction3;
+
+            Vector3 center = (p2 + p1) / 2;
+            Vector3 targetDirection = targetPoint - center;
+
+            // if d1 and d2 are diverging, targetPoint will be behind the center.
+            if (Vector3.Dot(targetDirection, direction1) < 0)
+            {
+                return -targetDirection;
+            }
+            return targetDirection;
+        }
+
+        private Vector3 GetCombiendDirection(Vector3 p1, Vector3 p2, Vector3 direction1, Vector3 direction2)
+        {
+            return GetCombiendDirection(p1, p2, direction1, direction2, Vector3.Cross(direction1, direction2));
+        }
 
 	// Update is called once per frame
 	void Update()
@@ -65,7 +112,9 @@ namespace ubc.ok.ovilab.HPUI.Core
                 {
                     newPosition = parent.position;
                 }
-                newRotation = Quaternion.LookRotation(parent.forward + secondParent.forward, parent.up + secondParent.up);
+
+                newRotation = Quaternion.LookRotation(GetCombiendDirection(parent.position, secondParent.position, parent.forward, secondParent.forward),
+                                                      GetCombiendDirection(parent.position, secondParent.position, parent.up, secondParent.up));
 	    }
 	    else
 	    {
