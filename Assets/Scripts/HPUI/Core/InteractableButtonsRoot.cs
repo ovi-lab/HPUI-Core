e8eda5c0226017af5ec7f4d638116a8f5305337a
Shariff Faleel <shariff.mfa@outlook.com>
2023-08-03 16:52:19 -0700

    Editor UI to trigger buttons

--- Assets/Scripts/HPUI/Core/InteractableButtonsRoot.cs
+++ Assets/Scripts/HPUI/Core/InteractableButtonsRoot.cs
@@ -3,5 +3,33 @@
 namespace ubc.ok.ovilab.HPUI.Core
 {
     public class InteractableButtonsRoot : MonoBehaviour
-    { }
+    {
+#if UNITY_EDITOR
+        private static GameObject dummyObject;
+
+        public void TriggerTargetButton(ButtonController targetButton)
+        {
+            if (dummyObject == null)
+            {
+                dummyObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                dummyObject.transform.localScale = Vector3.one * 0.02f;
+                dummyObject.GetComponent<MeshRenderer>().enabled = false;
+                dummyObject.AddComponent<ButtonTriggerCollider>();
+            }
+            if (targetButton == null)
+            {
+                return;
+            }
+
+            dummyObject.transform.position = targetButton.transform.position;
+            targetButton.contactZone.TriggerBehaviour(dummyObject.GetComponent<Collider>());
+            targetButton.contactAction.AddListener((btn) =>
+            {
+                dummyObject.transform.position = btn.transform.position - btn.transform.forward.normalized * 0.01f;
+                btn.contactZone.state = ButtonZone.State.outside;
+                btn.proximalZone.state = ButtonZone.State.outside;
+            });
+        }
+#endif
+    }
 }
---
+++ Assets/Scripts/HPUI/Editor/InteractableButtonsRootEditor.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using ubc.ok.ovilab.HPUI.Core;
+
+namespace ubc.ok.ovilab.HPUI.Editor
+{
+    [CustomEditor(typeof(InteractableButtonsRoot), true)]
+    public class InteractableButtonsRootEditor: UnityEditor.Editor
+    {
+        private InteractableButtonsRoot t;
+        private List<ButtonController> btns;
+
+        void OnEnable()
+        {
+            t = target as InteractableButtonsRoot;
+            btns = t.GetComponentsInChildren<ButtonController>().ToList();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.DrawDefaultInspector();
+            if (GUILayout.Button("Update buttons"))
+            {
+                btns = t.GetComponentsInChildren<ButtonController>().ToList();
+            }
+            GUI.enabled = EditorApplication.isPlaying;
+            EditorGUILayout.Space();
+            Rect rectBox = EditorGUILayout.BeginVertical("Box");
+            GUILayout.Label("Buttons", EditorStyles.boldLabel);
+            foreach (ButtonController btn in btns)
+            {
+                EditorGUILayout.BeginHorizontal();
+                string name = btn.transform.parent.parent.name;
+                EditorGUILayout.PrefixLabel(name);
+                if (GUILayout.Button("Trigger " + name))
+                {
+                    t.TriggerTargetButton(btn);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
