334ee4543c48048a2d9ea468e5ca01c10319a999
Shariff Faleel <shariff.mfa@outlook.com>
2023-12-20 17:54:04 -0800

    Base interactable/interactor setup

---
+++ Runtime/Interactions/HPUIBaseInteractable.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
+
+namespace ubco.ovi.HPUI.Core
+{
+    /// <summary>
+    /// Base HPUI interactable.
+    /// </summary>
+    [RequireComponent(typeof(JointFollower))]
+    [RequireComponent(typeof(XRPokeFilter))]
+    public class HPUIBaseInteractable: XRSimpleInteractable
+    {
+        private JointFollower jointFollower;
+        private Handedness handedness;
+        public Handedness Handedness
+        {
+            get {
+                if (jointFollower == null)
+                {
+                    jointFollower = GetComponent<JointFollower>();
+                }
+                handedness = jointFollower.handedness;
+                return handedness;
+            }
+        }
+    }
+}
---
+++ Runtime/Interactions/HPUITargetFilter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
+
+namespace ubco.ovi.HPUI.Core
+{
+    /// <summary>
+    /// Target filter that uses only the respective interactables.
+    /// </summary>
+    public class HPUITargetFilter : XRBaseTargetFilter
+    {
+        public Handedness handedness;
+
+        public override void Process(IXRInteractor interactor, List<IXRInteractable> targets, List<IXRInteractable> results)
+        {
+            if (handedness == Handedness.Invalid)
+            {
+                throw new InvalidOperationException("handedness not correcly set.");
+            }
+
+            results.Clear();
+            foreach(IXRInteractable target in targets)
+            {
+                if (target is HPUIBaseInteractable hpuiTarget && hpuiTarget.Handedness == handedness)
+                {
+                    results.Add(target);
+                }
+            }
+        }
+    }
+}
