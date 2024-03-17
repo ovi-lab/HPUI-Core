c485b46efa162db9e52fa04864a02f024a430594
Ahmed Shariff <shariff.mfa@outlook.com>
2024-03-17 13:20:16 -0700

    Adding a broken sample

---
+++ Samples~/SampleHPUIScene/Scripts/SampleInteractions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using ubco.ovilab.HPUI.Interaction;
+
+namespace ubco.ovilab.HPUI.Sample
+{
+    public class SampleInteractions: MonoBehaviour
+    {
+        public TextMeshPro text;
+
+        public void OnTap(HPUITapEventArgs args)
+        {
+            text.text = $"Tap @ {args.Position}";
+        }
+
+        public void OnGesture(HPUIGestureEventArgs args)
+        {
+            text.text = $"Gesture @ {args.Position} in {args.CumilativeDirection}";
+        }
+    }
+}
---
+++ Samples~/symlinkToSamples.ps1
@@ -0,0 +1,2 @@
+# To modify the sampels, create a symlink of the Samples~ directory
+New-Item -ItemType SymbolicLink -Path ../Samples -Target ../Samples~
