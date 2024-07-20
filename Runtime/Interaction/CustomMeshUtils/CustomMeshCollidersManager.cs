a9f35a5348342a47a0799456299d4ea46fc6125f
GAmuzak <rishavbanerjee1710@gmail.com>
2024-07-19 18:58:12 -0700

    placing, orienting and scaling during creation

--- Runtime/Interaction/CustomMeshUtils/CustomMeshCollidersManager.cs
+++ Runtime/Interaction/CustomMeshUtils/CustomMeshCollidersManager.cs
@@ -15,29 +15,53 @@ namespace ubco.ovilab.HPUI.CustomMeshUtils
         // [SerializeField] private GameObject rectifiedVertexDS;
         // [SerializeField, Range(0, 120)] private int id;
         
-        private NativeArray<Vector3> verticesNative, normalsNative;
+        private NativeArray<Vector3> vertices_native, normals_native;
         private List<Vector3> vertices = new List<Vector3>(), normals = new List<Vector3>();
-        private NativeArray<int> remappedVertices;
+        private NativeArray<int> remapped_vertices_data;
         private Mesh tempMesh;
         private bool generatedColliders;
 
-        public void SetupColliders(SkinnedMeshRenderer keyboardMesh)
+        public void SetupColliders(SkinnedMeshRenderer keyboardMesh, HPUICustomMesh hpuiCustomMesh)
         {
             tempMesh = new Mesh(); 
             keyboardMesh.BakeMesh(tempMesh, true);
+            if (vertexRemapData == null)
+            {
+                throw new ArgumentException("Missing Vertex Remap Data Asset! Create a new one or provide an existing one!");
+            }
             if (vertexRemapData.RemappedVertices.Length <= 0)
             {
                 VertexRemapper.GetRectifiedIndices(vertexRemapData, tempMesh);
             }
             tempMesh.GetVertices(vertices);
             tempMesh.GetNormals(normals);
-            if (!remappedVertices.IsCreated)
+            if (!remapped_vertices_data.IsCreated)
             {
-                remappedVertices = new NativeArray<int>(vertexRemapData.RemappedVertices, Allocator.Persistent);
+                remapped_vertices_data = new NativeArray<int>(vertexRemapData.RemappedVertices, Allocator.Persistent);
             }
             else
             {
-                remappedVertices.CopyFrom(vertexRemapData.RemappedVertices);
+                remapped_vertices_data.CopyFrom(vertexRemapData.RemappedVertices);
+            }
+
+            float xWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[1]]);
+            float yWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[hpuiCustomMesh.MeshXRes]]);
+            
+            Transform meshTransform = keyboardMesh.gameObject.transform;
+            for (int i = 0; i < remapped_vertices_data.Length; i++)
+            {
+                int x = i % hpuiCustomMesh.MeshXRes;
+                int y = i / hpuiCustomMesh.MeshXRes;
+                GameObject col = new GameObject();
+                col.AddComponent<BoxCollider>();
+                col.name = "X: " + x + "; Y: " + y + ";";
+                col.transform.parent = meshTransform;
+                col.transform.localPosition = vertices[remapped_vertices_data[i]];
+                Vector3 targetScale = Vector3.one * 0.0001f;
+                targetScale.x = xWidth;
+                targetScale.y = yWidth;
+                col.transform.localScale = targetScale;
+                col.transform.localRotation = Quaternion.LookRotation(normals[remapped_vertices_data[i]]);
             }
             
             generatedColliders = true;
@@ -49,6 +73,14 @@ namespace ubco.ovilab.HPUI.CustomMeshUtils
             
             
             // rectifiedVertexDS.transform.localPosition = vertices[remappedVertices[id]];
+        }
+
+        private void OnDrawGizmos()
+        {
+            foreach (int t in remapped_vertices_data)
+            {
+                Debug.DrawRay(transform.TransformPoint(vertices[t]), Vector3.Normalize(normals[t]));
+            }
         }
     }
 }
