ecac19b87150400d9312aa6a8fe02ff5c8843ac3
GAmuzak <rishavbanerjee1710@gmail.com>
2024-07-19 22:15:55 -0700

    added jobs system implementation for updating positions

--- Runtime/Interaction/CustomMeshUtils/CustomMeshCollidersManager.cs
+++ Runtime/Interaction/CustomMeshUtils/CustomMeshCollidersManager.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 namespace ubco.ovilab.HPUI.CustomMeshUtils
 {
@@ -18,68 +20,124 @@ namespace ubco.ovilab.HPUI.CustomMeshUtils
         private NativeArray<Vector3> vertices_native, normals_native;
         private List<Vector3> vertices = new List<Vector3>(), normals = new List<Vector3>();
         private NativeArray<int> remapped_vertices_data;
+        private TransformAccessArray colliderObjects;
         private Mesh tempMesh;
         private bool generatedColliders;
+        private SkinnedMeshRenderer targetMesh;
+        private float scaleFactor = 0.001f;
+        private int meshXRes, meshYRes;
+        
+        private void Update()
+        {
+            if (!generatedColliders || !isActiveAndEnabled) return;
+
+            UpdateColliderPositions();
+            // rectifiedVertexDS.transform.localPosition = vertices[remappedVertices[id]];
+        }
+
+        private void OnDestroy()
+        {
+            vertices_native.Dispose();
+            normals_native.Dispose();
+            remapped_vertices_data.Dispose();
+        }
 
         public void SetupColliders(SkinnedMeshRenderer keyboardMesh, HPUICustomMesh hpuiCustomMesh)
         {
+            targetMesh = keyboardMesh;
             tempMesh = new Mesh(); 
-            keyboardMesh.BakeMesh(tempMesh, true);
+            targetMesh.BakeMesh(tempMesh, true);
+            
             if (vertexRemapData == null)
             {
                 throw new ArgumentException("Missing Vertex Remap Data Asset! Create a new one or provide an existing one!");
             }
+            
             if (vertexRemapData.RemappedVertices.Length <= 0)
             {
                 VertexRemapper.GetRectifiedIndices(vertexRemapData, tempMesh);
             }
+            
             tempMesh.GetVertices(vertices);
             tempMesh.GetNormals(normals);
-            if (!remapped_vertices_data.IsCreated)
-            {
-                remapped_vertices_data = new NativeArray<int>(vertexRemapData.RemappedVertices, Allocator.Persistent);
-            }
-            else
-            {
-                remapped_vertices_data.CopyFrom(vertexRemapData.RemappedVertices);
-            }
-
-            float xWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[1]]);
-            float yWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[hpuiCustomMesh.MeshXRes]]);
             
-            Transform meshTransform = keyboardMesh.gameObject.transform;
+            remapped_vertices_data = new NativeArray<int>(vertexRemapData.RemappedVertices, Allocator.Persistent);
+            
+            
+            meshXRes = hpuiCustomMesh.MeshXRes;
+            meshYRes = vertices.Count / meshXRes;
+            
+            float xWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[1]]);
+            float yWidth = Vector3.Distance(vertices[remapped_vertices_data[0]], vertices[remapped_vertices_data[meshXRes]]);
+            Transform[] colliderTransforms = new Transform[vertices.Count];
+            Transform meshTransform = targetMesh.gameObject.transform;
             for (int i = 0; i < remapped_vertices_data.Length; i++)
             {
-                int x = i % hpuiCustomMesh.MeshXRes;
-                int y = i / hpuiCustomMesh.MeshXRes;
+                int x = i % meshXRes;
+                int y = i / meshXRes;
                 GameObject col = new GameObject();
                 col.AddComponent<BoxCollider>();
                 col.name = "X: " + x + "; Y: " + y + ";";
                 col.transform.parent = meshTransform;
                 col.transform.localPosition = vertices[remapped_vertices_data[i]];
-                Vector3 targetScale = Vector3.one * 0.0001f;
+                Vector3 targetScale = Vector3.one * scaleFactor;
                 targetScale.x = xWidth;
                 targetScale.y = yWidth;
                 col.transform.localScale = targetScale;
                 col.transform.localRotation = Quaternion.LookRotation(normals[remapped_vertices_data[i]]);
+                colliderTransforms[i] = col.transform;
             }
-            
+            colliderObjects = new TransformAccessArray(colliderTransforms);
+            vertices_native = new NativeArray<Vector3>(vertices.ToArray(), Allocator.Persistent);
+            normals_native = new NativeArray<Vector3>(normals.ToArray(), Allocator.Persistent);
             generatedColliders = true;
         }
 
-        private void Update()
+        
+        protected void UpdateColliderPositions()
         {
-            if (!generatedColliders) return;
-            
-            
-            // rectifiedVertexDS.transform.localPosition = vertices[remappedVertices[id]];
-        }
+            targetMesh.BakeMesh(tempMesh, true);
+            tempMesh.GetVertices(vertices);
+            tempMesh.GetNormals(normals);
 
-        private void OnDrawGizmos()
-        {
-            foreach (int t in remapped_vertices_data)
+            vertices_native.CopyFrom(vertices.ToArray());
+            normals_native.CopyFrom(normals.ToArray());
+            
+            DeformedCollidersJob job = new DeformedCollidersJob()
             {
-                Debug.DrawRay(transform.TransformPoint(vertices[t]), Vector3.Normalize(normals[t]));
+                Normals = normals_native,
+                Vertices = vertices_native,
+                RemappedIndices = remapped_vertices_data,
+                MaxX = meshXRes,
+                MaxY = meshYRes,
+            };
+
+            JobHandle jobHandle = job.Schedule(colliderObjects);
+            jobHandle.Complete();
+        }
+        
+        struct DeformedCollidersJob: IJobParallelForTransform
+        {
+            private Vector3 right, upwards, temppos;
+            public float ScaleFactor, GridSize; 
+            public int MaxX, MaxY;
+
+            [ReadOnly] public NativeArray<Vector3> Vertices;
+            [ReadOnly] public NativeArray<Vector3> Normals;
+            [ReadOnly] public NativeArray<int> RemappedIndices;
+	
+            public void Execute(int i, TransformAccess col)
+            {
+                temppos = Vertices[RemappedIndices[i]];
+                temppos.z += -0.0002f;
+
+                if (i >= (MaxY-1) * MaxX )
+                    upwards = Vertices[RemappedIndices[i - MaxX]] - Vertices[RemappedIndices[i]];
+                else
+                    upwards = Vertices[RemappedIndices[i+ MaxX]] - Vertices[RemappedIndices[i]];
+                
+                col.localPosition = temppos;
+                col.localRotation = Quaternion.LookRotation(Normals[RemappedIndices[i]], upwards);
             }
         }
     }
