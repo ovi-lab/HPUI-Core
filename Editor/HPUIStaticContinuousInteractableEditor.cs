67e37a98013fa4f85c1038a642d0bbde2db06d7b
GAmuzak <rishavbanerjee1710@gmail.com>
2024-07-30 13:25:30 -0700

    api assumption fix fr fr no cap on a stack sheesh

--- Editor/HPUIStaticContinuousInteractableEditor.cs
+++ Editor/HPUIStaticContinuousInteractableEditor.cs
@@ -12,13 +12,11 @@ namespace ubco.ovilab.HPUI.Editor
         private HPUIStaticContinuousInteractable t;
         private SerializedProperty staticMesh;
         private SerializedProperty meshXResolution;
-        private SerializedProperty flippedAndNormalisedCoords;
         
         protected override List<string> EventPropertyNames => base.EventPropertyNames.Union(new List<string>()
         {
             "staticMesh",
             "meshXResolution",
-            "flippedAndNormalisedCoords"
         }).ToList();
         
         protected override void OnEnable()
@@ -27,7 +25,6 @@ namespace ubco.ovilab.HPUI.Editor
             t = (HPUIStaticContinuousInteractable)target;
             staticMesh = serializedObject.FindProperty("staticHPUIMesh");
             meshXResolution = serializedObject.FindProperty("meshXResolution");
-            flippedAndNormalisedCoords = serializedObject.FindProperty("flippedAndNormalisedCoords");
         }
 
         protected override void DrawProperties()
@@ -37,7 +34,6 @@ namespace ubco.ovilab.HPUI.Editor
             EditorGUILayout.LabelField("Static Mesh Configurations", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(staticMesh);
             EditorGUILayout.PropertyField(meshXResolution);
-            EditorGUILayout.PropertyField(flippedAndNormalisedCoords);
         }
     }
 }
