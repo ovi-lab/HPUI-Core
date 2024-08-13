996f3d2daac66e2b5403ef5637eda23fa005648f
GAmuzak <rishavbanerjee1710@gmail.com>
2024-08-12 17:15:26 -0700

    fixed sfield data assignment

--- Editor/StaticMeshCollidersManagerEditor.cs
+++ Editor/StaticMeshCollidersManagerEditor.cs
@@ -12,14 +12,28 @@ namespace ubco.ovilab.HPUI.Editor
 
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
+
+            SerializedProperty vertRemapData = serializedObject.FindProperty("vertexRemapData");
             t = (StaticMeshCollidersManager)target;
+
             GUIContent buttonContent = new GUIContent("Compute Vertex Remapping", "Run this with the hand upright and a blank Vertex Remap Data asset assigned outside of play mode");
-            if(GUILayout.Button(buttonContent))
+
+            if (GUILayout.Button(buttonContent))
             {
-                t.RemapVertices();
+                int[] remapData = t.RemapVertices(); 
+
+                vertRemapData.arraySize = remapData.Length;
+
+                for (int i = 0; i < remapData.Length; i++)
+                {
+                    vertRemapData.GetArrayElementAtIndex(i).intValue = remapData[i];
+                }
             }
 
+            serializedObject.ApplyModifiedProperties();
             DrawDefaultInspector();
         }
+
     }
 }
