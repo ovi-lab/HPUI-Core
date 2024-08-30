b4c248f8ab629b1f37e15b5013f4d0ae63b07d3f
Ahmed Shariff <shariff.mfa@outlook.com>
2024-08-29 22:50:08 -0700

    Fix rotation angle computation for rays

--- Runtime/Interaction/HPUIInteractorRayAngle.cs
+++ Runtime/Interaction/HPUIInteractorRayAngle.cs
@@ -14,14 +14,51 @@ namespace ubco.ovilab.HPUI.Interaction
             this.z = z;
         }
 
+        // /// <summary>
+        // /// Get a quaternion that would rotate x angles around its right axis and z angles around its forward axis.
+        // /// </summary>
+        // private static Quaternion GetRotation(float x, float z)
+        // {
+        //     float yDist = Mathf.Sqrt(1 / (1 + Mathf.Pow(Mathf.Tan(x * Mathf.Deg2Rad), 2) + Mathf.Pow(Mathf.Tan(z * Mathf.Deg2Rad), 2)));
+        //     if (Mathf.Abs(x) > 90 || Mathf.Abs(z) > 90)
+        //     {
+        //         yDist = -yDist;
+        //     }
+
+        //     float xDist = Mathf.Tan(x * Mathf.Deg2Rad) * yDist;
+        //     float zDist = Mathf.Tan(z * Mathf.Deg2Rad) * yDist;
+
+        //     Vector3 newUp = Vector3.up * yDist + Vector3.right * xDist + Vector3.forward * zDist;
+
+        //     // Don't care about the forward axis, as long the the up vector gets to the right postion
+        //     Vector3 newforward = Vector3.Cross(newUp, Quaternion.Euler(0, 0, 90) * newUp);
+        //     Vector3 newRight = Vector3.Cross(newforward, newUp);
+        //     Debug.Log($"{Vector3.Dot(newUp, Vector3.Cross(newRight, newforward))}");
+
+        //     return Quaternion.LookRotation(newUp, newforward);
+        //     // return Quaternion.LookRotation(Vector3.up, newUp);
+        // }
+
+        /// <summary>
+        /// The direction relatetive to the unity up, forward and right vectors.
+        /// X is the angle from the up vector around the right vector.
+        /// Z is the angle from the up vector around the forward vector.
+        /// </summary>
         public static Vector3 GetDirection(float x, float z, Vector3 right, Vector3 forward, Vector3 up, bool flipZAngles)
         {
             float x_ = x,
                   z_ = flipZAngles ? -z : z;
 
-            Quaternion rotation = Quaternion.AngleAxis(x_, right) * Quaternion.AngleAxis(z_, forward);
-            return rotation * up;
-            // return 
+            float yDist = Mathf.Sqrt(1 / (1 + Mathf.Pow(Mathf.Tan(x * Mathf.Deg2Rad), 2) + Mathf.Pow(Mathf.Tan(z * Mathf.Deg2Rad), 2)));
+            if (Mathf.Abs(x) > 90 || Mathf.Abs(z) > 90)
+            {
+                yDist = -yDist;
+            }
+
+            float xDist = Mathf.Tan(x * Mathf.Deg2Rad) * yDist;
+            float zDist = Mathf.Tan(z * Mathf.Deg2Rad) * yDist;
+
+            return yDist * up + zDist * forward + xDist * right;
         }
 
         public Vector3 GetDirection(Transform attachTransform, bool flipZAngles)
