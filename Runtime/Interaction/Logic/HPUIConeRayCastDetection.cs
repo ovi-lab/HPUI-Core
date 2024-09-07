a7766ac0dcf6eb0111800ca362a8b8f50139623c
Shariff Faleel <shariff.mfa@outlook.com>
2024-09-06 17:57:16 -0700

    Setup for refactoring detection techniques

---
+++ Runtime/Interaction/Logic/HPUIConeRayCastDetection.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace ubco.ovilab.HPUI.Interaction
+{
+    /// <summary>
+    /// </summary>
+    // public class HPUIConeRayCastDetection: IHPUIDetectInteractables
+    // {
+    // }
+}
