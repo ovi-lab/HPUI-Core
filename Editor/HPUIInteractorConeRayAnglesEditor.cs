d9e9904b62f98849267d01bd7defad71960ad76f
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-02-17 18:29:55 -0800

    creating foldout group, adding more warnings

--- Editor/HPUIInteractorConeRayAnglesEditor.cs
+++ Editor/HPUIInteractorConeRayAnglesEditor.cs
@@ -24,6 +24,8 @@ namespace ubco.ovilab.HPUI.Editor
 
         private const string DONT_ASK_FOR_OVERRIDE_EDITOR_PREF_KEY = "ubco.ovilab.HPUI.Interaction.Components.ConeThresholdsEditor.DontAskForOverride";
 
+        private bool advancedSettingsFoldout;
+
         protected void OnEnable()
         {
             t = target as HPUIInteractorConeRayAngles;
@@ -39,58 +41,44 @@ namespace ubco.ovilab.HPUI.Editor
 
         public override void OnInspectorGUI()
         {
-            EditorGUILayout.LabelField("Apply Ray Selection Threshold Offset", EditorStyles.boldLabel);
-            offsetValue = EditorGUILayout.FloatField("Offset", offsetValue);
-            applyToAll = EditorGUILayout.Toggle("Apply to All", applyToAll);
-            overwriteThisAsset = EditorGUILayout.Toggle("Overwrite This Asset", overwriteThisAsset);
+            base.OnInspectorGUI();
 
-            EditorGUI.BeginDisabledGroup(applyToAll);
-            selectedPhalange = (XRHandJointID)EditorGUILayout.EnumPopup("Phalange", selectedPhalange);
-            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.Space();
 
-            if (GUILayout.Button(overwriteThisAsset ? "Overwrite this Asset and Apply Offset" : "Create Copy and Apply Offset"))
+            advancedSettingsFoldout = EditorGUILayout.Foldout(advancedSettingsFoldout, "Advanced Settings", toggleOnLabelClick:true);
+            if(advancedSettingsFoldout)
             {
-                Undo.RecordObject(t, "Apply RaySelectionThreshold Offset");
+                EditorGUILayout.LabelField("Apply Ray Selection Threshold Offset", EditorStyles.boldLabel);
+                offsetValue = EditorGUILayout.FloatField("Offset", offsetValue);
+                applyToAll = EditorGUILayout.Toggle("Apply to All", applyToAll);
+                overwriteThisAsset = EditorGUILayout.Toggle("Overwrite This Asset", overwriteThisAsset);
 
-                if (applyToAll)
+                EditorGUI.BeginDisabledGroup(applyToAll);
+                selectedPhalange = (XRHandJointID)EditorGUILayout.EnumPopup("Phalange", selectedPhalange);
+                EditorGUI.EndDisabledGroup();
+
+                if (overwriteThisAsset) EditorGUILayout.HelpBox("You are about to overwrite this asset!", MessageType.Warning); // warning
+                if(overwriteThisAsset) GUI.backgroundColor = Color.red; // even more warning
+                if (GUILayout.Button(overwriteThisAsset ? "Overwrite this Asset and Apply Offset" : "Create Copy and Apply Offset"))
                 {
-                    if(overwriteThisAsset)
-                    {
-                        bool result = EditorUtility.DisplayDialog("Overwrite Cone Ray Angles Asset", "Are you sure you want to overwrite this asset?\nYou can undo or set offsets to 0 to regain original values.\nOnce you overwrite and close the editor the values will be permanently modified!", "Yes", "No", DialogOptOutDecisionType.ForThisSession, DONT_ASK_FOR_OVERRIDE_EDITOR_PREF_KEY);
-                        if(result)
-                        {
-                            foreach ((XRHandJointID phalange, List<HPUIInteractorRayAngle> rayAngle) in t.ActiveFingerAngles)
-                            {
-                                ApplyOffsetToAsset(phalange, rayAngle);
-                            }
-                            EditorUtility.SetDirty(t);
-                            AssetDatabase.SaveAssets();
-                            AssetDatabase.Refresh();
-                        }
-                    }
-                    else
-                    {
-                        HPUIInteractorConeRayAngles newAsset = Instantiate(t);
-                        foreach ((XRHandJointID phalange, List<HPUIInteractorRayAngle> rayAngle) in newAsset.ActiveFingerAngles)
-                        {
-                            ApplyOffsetToAsset(phalange, rayAngle);
-                        }
-                        string saveName = EditorUtility.SaveFilePanelInProject("Save new cone angles asset", $"{t.name}_copy_all_{offsetValue}", "asset", "Save location for the modified cone angle asset.");
-                        AssetDatabase.CreateAsset(newAsset, saveName);
-                        EditorUtility.SetDirty(newAsset);
-                        AssetDatabase.SaveAssets();
-                    }
-                }
-                else
-                {
-                    if (t.ActiveFingerAngles.TryGetValue(selectedPhalange, out List<HPUIInteractorRayAngle> rayAngles))
+                    Undo.RecordObject(t, "Apply RaySelectionThreshold Offset");
+
+                    if (applyToAll)
                     {
                         if (overwriteThisAsset)
                         {
-                            bool result = EditorUtility.DisplayDialog("Overwrite Cone Ray Angles Asset", "Are you sure you want to overwrite this asset?\nYou can undo or set offsets to 0 to regain original values.\nOnce you overwrite and close the editor the values will be permanently modified!", "Yes", "No", DialogOptOutDecisionType.ForThisSession, DONT_ASK_FOR_OVERRIDE_EDITOR_PREF_KEY);
+                            bool result = EditorUtility.DisplayDialog("Overwrite Cone Ray Angles Asset",
+                                "Are you sure you want to overwrite this asset?\nYou can undo or set offsets to 0 to regain original values.\nOnce you overwrite and close the editor the values will be permanently modified!",
+                                "Yes", "No", DialogOptOutDecisionType.ForThisSession,
+                                DONT_ASK_FOR_OVERRIDE_EDITOR_PREF_KEY);
                             if (result)
                             {
-                                ApplyOffsetToAsset(selectedPhalange, rayAngles);
+                                foreach ((XRHandJointID phalange, List<HPUIInteractorRayAngle> rayAngle) in t
+                                             .ActiveFingerAngles)
+                                {
+                                    ApplyOffsetToAsset(phalange, rayAngle);
+                                }
+
                                 EditorUtility.SetDirty(t);
                                 AssetDatabase.SaveAssets();
                                 AssetDatabase.Refresh();
@@ -99,10 +87,15 @@ namespace ubco.ovilab.HPUI.Editor
                         else
                         {
                             HPUIInteractorConeRayAngles newAsset = Instantiate(t);
-                            newAsset.ActiveFingerAngles.TryGetValue(selectedPhalange, out List<HPUIInteractorRayAngle> newRayAngles);
-                            Debug.Assert(newRayAngles!=null, "New asset copy doesn't have the same data as the old asset??");
-                            ApplyOffsetToAsset(selectedPhalange, newRayAngles);
-                            string saveName = EditorUtility.SaveFilePanelInProject("Save new cone angles asset", $"{t.name}_copy_{selectedPhalange}_{offsetValue}", "asset", "Save location for the modified cone angle asset.");
+                            foreach ((XRHandJointID phalange, List<HPUIInteractorRayAngle> rayAngle) in newAsset
+                                         .ActiveFingerAngles)
+                            {
+                                ApplyOffsetToAsset(phalange, rayAngle);
+                            }
+
+                            string saveName = EditorUtility.SaveFilePanelInProject("Save new cone angles asset",
+                                $"{t.name}_copy_all_{offsetValue}", "asset",
+                                "Save location for the modified cone angle asset.");
                             AssetDatabase.CreateAsset(newAsset, saveName);
                             EditorUtility.SetDirty(newAsset);
                             AssetDatabase.SaveAssets();
@@ -110,14 +103,50 @@ namespace ubco.ovilab.HPUI.Editor
                     }
                     else
                     {
-                        Debug.LogWarning("Selected phalange not found in ActiveFingerAngles.");
+                        if (t.ActiveFingerAngles.TryGetValue(selectedPhalange,
+                                out List<HPUIInteractorRayAngle> rayAngles))
+                        {
+                            if (overwriteThisAsset)
+                            {
+                                bool result = EditorUtility.DisplayDialog("Overwrite Cone Ray Angles Asset",
+                                    "Are you sure you want to overwrite this asset?\nYou can undo or set offsets to 0 to regain original values.\nOnce you overwrite and close the editor the values will be permanently modified!",
+                                    "Yes", "No", DialogOptOutDecisionType.ForThisSession,
+                                    DONT_ASK_FOR_OVERRIDE_EDITOR_PREF_KEY);
+                                if (result)
+                                {
+                                    ApplyOffsetToAsset(selectedPhalange, rayAngles);
+                                    EditorUtility.SetDirty(t);
+                                    AssetDatabase.SaveAssets();
+                                    AssetDatabase.Refresh();
+                                }
+                            }
+                            else
+                            {
+                                HPUIInteractorConeRayAngles newAsset = Instantiate(t);
+                                newAsset.ActiveFingerAngles.TryGetValue(selectedPhalange,
+                                    out List<HPUIInteractorRayAngle> newRayAngles);
+                                Debug.Assert(newRayAngles != null,
+                                    "New asset copy doesn't have the same data as the old asset??");
+                                ApplyOffsetToAsset(selectedPhalange, newRayAngles);
+                                string saveName = EditorUtility.SaveFilePanelInProject("Save new cone angles asset",
+                                    $"{t.name}_copy_{selectedPhalange}_{offsetValue}", "asset",
+                                    "Save location for the modified cone angle asset.");
+                                AssetDatabase.CreateAsset(newAsset, saveName);
+                                EditorUtility.SetDirty(newAsset);
+                                AssetDatabase.SaveAssets();
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogWarning("Selected phalange not found in ActiveFingerAngles.");
+                        }
                     }
                 }
             }
 
-            EditorGUILayout.Space();
 
-            base.OnInspectorGUI();
+
+
         }
 
         /// <summary>
