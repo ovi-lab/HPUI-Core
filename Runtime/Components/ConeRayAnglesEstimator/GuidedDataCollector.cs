15659ccabb61052d69311de6cbfdf212dfd429d3
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-06-11 17:19:28 -0700

    ensures unique data recording per phalange if bool enabled

--- Runtime/Components/ConeRayAnglesEstimator/GuidedDataCollector.cs
+++ Runtime/Components/ConeRayAnglesEstimator/GuidedDataCollector.cs
@@ -16,12 +16,32 @@ namespace ubco.ovilab.HPUI
         /// </summary>
         public HPUIInteractorConeRayAngleSegment TargetSegment { get => targetSegment; set => targetSegment = value; }
 
+        [SerializeField]
+        [Tooltip("Ensures that only one calibration data record is collected for each phalange. Disabling this will allow averaging over multiple calibrations per phalange")]
+        private bool uniqueDataRecordPerPhalange = true;
+
+        /// <summary>
+        /// Ensures that only one calibration data record is collected for each phalange. Disabling this will allow averaging over multiple calibrations per phalange
+        /// </summary>
+        public bool UniqueDataRecordPerPhalange { get => uniqueDataRecordPerPhalange; set => uniqueDataRecordPerPhalange = value; }
+
         /// <summary>
         /// This will create a <see cref="ConeRayComputationDataRecord"/> for the
         /// segment passed as a parameter.
         /// </summary>
         public void EndCalibrationForSegment(HPUIInteractorConeRayAngleSegment segment)
         {
+            if (uniqueDataRecordPerPhalange)
+            {
+                foreach (var dataRecord in DataRecords)
+                {
+                    if (dataRecord.segment == segment)
+                    {
+                        DataRecords.Remove(dataRecord);
+                    }
+                }
+            }
+
             DataRecords.Add(new ConeRayComputationDataRecord(currentInteractionData, segment));
 
             currentInteractionData = new();
