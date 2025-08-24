ab57a4ed20db22c7168e5686c8ab0f460bdc442c
Rishav Banerjee <rishavbanerjee1710@gmail.com>
2025-08-24 16:28:08 -0700

    bursting mesh continuous colliders manager (#50)
    gives a nice 10-15 fps boost overall
    could probably be improved with the use of mathematics datatypes like
    float3 instead of Vector3, let me know if that's worth implementing

--- Runtime/Interaction/MeshContinuousCollidersManager.cs
+++ Runtime/Interaction/MeshContinuousCollidersManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
@@ -157,6 +158,7 @@ namespace ubco.ovilab.HPUI.Interaction
             return coordsForCol;
         }
 
+        [BurstCompile]
         protected void UpdateColliderPositions()
         {
             mesh.BakeMesh(tempMesh, true);
@@ -179,6 +181,7 @@ namespace ubco.ovilab.HPUI.Interaction
             jobHandle.Complete();
         }
 
+        [BurstCompile]
         struct DeformedCollidersJob: IJobParallelForTransform
         {
             private Vector3 right, forward, temppos;
